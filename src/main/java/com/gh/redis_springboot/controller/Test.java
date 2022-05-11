681ae840cfefa2ae26035af409c758137f87301f
aguo <353936905@qq.com>
2022-05-11 15:05:43 +0800

    这是第二次测试提交。

--- src/main/java/com/gh/redis_springboot/controller/Test.java
+++ src/main/java/com/gh/redis_springboot/controller/Test.java
@@ -8,5 +8,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("这是一个测试！！！");
+        System.out.println("这是一个测试！！！");
+        System.out.println("这是一个测试！！！");
     }
 }
